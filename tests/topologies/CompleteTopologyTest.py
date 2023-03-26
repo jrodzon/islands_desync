42fa34b48d6d2189069de84a001eb23e126fed3e
youngdashu <anowak2000@gmail.com>
2023-03-27 01:15:41 +0200

    added CompleteTopology.py

---
+++ tests/topologies/CompleteTopologyTest.py
@@ -0,0 +1,28 @@
+import unittest
+
+from src.topologies.CompleteTopology import CompleteTopology
+
+
+class MyTestCase(unittest.TestCase):
+    def test_general(self):
+        self.assertEqual(
+            {
+                0: [1, 2, 3],
+                1: [0, 2, 3],
+                2: [0, 1, 3],
+                3: [0, 1, 2],
+            },
+            CompleteTopology(4).create()
+        )
+
+    def test_small_case(self):
+        self.assertEqual(
+            {
+                0: []
+            },
+            CompleteTopology(1).create()
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
