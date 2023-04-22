36dae20143b1f7e33df4e87d50ce6da5dc186060
youngdashu <anowak2000@gmail.com>
2023-04-22 17:13:17 +0200

    added genetic algorithm

---
+++ src/geneticAlgorithm/generator/island_solution_generator.py
@@ -0,0 +1,28 @@
+from typing import TypeVar
+
+from jmetal.core.problem import Problem
+from jmetal.util.generator import Generator
+from jmetal.core.solution import FloatSolution, BinarySolution
+from solution.float_island_solution import FloatIslandSolution
+from solution.binary_island_solution import BinaryIslandSolution
+R = TypeVar('R')
+
+
+class IslandSolutionGenerator(Generator):
+    def __init__(self, island_number):
+        self.island_number = island_number
+
+    def new(self, problem: Problem):
+        solution = problem.create_solution()
+        if isinstance(solution, FloatSolution):
+            return FloatIslandSolution(solution.lower_bound, solution.upper_bound, solution.number_of_objectives, solution.number_of_constraints,
+                                      solution.variables, solution.objectives, solution.constraints,
+                                      from_island=self.island_number, from_evaluation=0)
+        else:
+            return solution
+        """elif isinstance(solution, BinarySolution):
+                    return BinaryIslandSolution(solution.number_of_variables, solution.number_of_objectives, solution.number_of_constraints,
+                                              solution.variables, solution.objectives, solution.constraints,
+                                              from_island=self.island_number, from_evaluation=0)"""
+
+
